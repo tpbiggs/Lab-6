47f7353b73a82f3c7828141426fc36c7c4e4be61
tpbig <tpbig@DESKTOP-3T6GCV8>
2019-09-29 13:22:55 -0500

    finished work

--- mavenproject1/src/main/java/oddCounter.java
+++ mavenproject1/src/main/java/oddCounter.java
@@ -1,13 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+import java.util.Random;
 /**
  *
  * @author tpbig
  */
 public class oddCounter {
+public static void main(String[] args){
+    Random rand = new Random();
+    int i;
+    int num;
+    int odd = 0;
+    int even = 0;
     
+    for(i=0;i < 100; ++i){
+        num = rand.nextInt(1000)+1;
+        if (oddCheck(num) == true){
+        System.out.println(num +" is odd.");
+        ++odd;
+         }
+         else {
+        System.out.println(num + " is even.");
+            ++even;
+        }
+    
+    }
+    System.out.println("Odd: " + odd + "\n" + "Even: " + even);
+    
+}
+
+public static boolean oddCheck(int number) {
+    return number%2 != 0;
+}
 }
