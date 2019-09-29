7a1b970a2a24b6f531ebd4f0dc76b0a606dd783f
tpbig <tpbig@DESKTOP-3T6GCV8>
2019-09-29 02:34:42 -0500

    saturday work

--- mavenproject1/src/main/java/isPrime.java
+++ mavenproject1/src/main/java/isPrime.java
@@ -1,3 +1,9 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +15,28 @@
  * @author tpbig
  */
 public class isPrime {
-    
+     public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        int userNum = keyboard.nextInt();
+        if(checkPrime(userNum)==true){
+            System.out.println("Prime Number");
+        }
+        else{
+            System.out.println("Not Prime Number");
+        }
+        
+    }
+ public static boolean checkPrime(int checkNum){
+    for(int i=2;i<=checkNum/2;i++)
+	{
+        int temp = checkNum%i;
+	   if(temp==0)
+	   {
+            return false;
+	   }
+	}
+    return true;
 }
+
+}
+
