3545803f42a34fba4b8672009eadc6bf49f4d5cd
tpbig <tpbig@DESKTOP-3T6GCV8>
2019-10-11 17:47:40 -0500

    finished

--- Lab4/src/main/java/main.java
+++ Lab4/src/main/java/main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,30 @@
  */
 public class main {
     public static void main(String args[]){
+        Scanner keyboard = new Scanner(System.in);
         
+//car section start
+        Car myCar = new Car(1987, "Toyota Speedster AE86");
+        
+        for (int i = 0; i < 5; ++i){
+            myCar.gas();
+            System.out.println("Speed: " + myCar.getSpeed());
+        }
+        
+        for (int i = 0; i < 5; ++i){
+            myCar.brake();
+            System.out.println("Speed: " + myCar.getSpeed());
+        }
+//car section end
+
+//circle section start
+        System.out.println("Radius: ");
+        double rad = keyboard.nextDouble();
+        
+        Circle myCircle = new Circle(rad);
+        
+        System.out.println("Area: " + myCircle.area());
+        System.out.println("Diameter: " + myCircle.diameter());
+        System.out.println("Circumference: " + myCircle.circumference());
     }
 }
