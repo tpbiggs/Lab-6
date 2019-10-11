6842e31aca83a6e7379e6b01584e8c661b1e8489
tpbig <tpbig@DESKTOP-3T6GCV8>
2019-10-11 12:31:50 -0500

    finished class

--- Lab4/src/main/java/Circle.java
+++ Lab4/src/main/java/Circle.java
@@ -9,5 +9,31 @@
  * @author tpbig
  */
 public class Circle {
+    private double radius;
+    private double PI = 3.14159;
+    
+    public Circle(){
+        radius = 0;
+    }
+    public Circle(double rad){
+        radius = rad;
+    }
+    
+    public void setRadius(double rad){
+        radius = rad;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    
+    public double area(){
+        return (PI* radius * radius);
+    }
+    public double diameter(){
+        return (radius * 2);
+    }
+    public double circumference(){
+        return (2 * radius * PI);
+    }
     
 }
