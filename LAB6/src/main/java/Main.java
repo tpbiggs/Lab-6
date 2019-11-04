102d518e0f18aea15eb4f89c68a70cc8271977ee
tpbig <tpbig@DESKTOP-3T6GCV8>
2019-11-03 22:36:27 -0600

    finished

--- LAB6/src/main/java/Main.java
+++ LAB6/src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,26 @@
  */
 public class Main {
     public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Length 1: ");
+        int length1 = keyboard.nextInt();
+        System.out.println("Width 1: ");
+        int width1 = keyboard.nextInt();
+        System.out.println("Length 2: ");
+        int length2 = keyboard.nextInt();
+        System.out.println("Width 2: ");
+        int width2 = keyboard.nextInt();
+        
+        LandTract plot1 = new LandTract(length1, width1);
+        LandTract plot2 = new LandTract(length2, width2);
+        
+        if (plot1.equalArea(plot2) == true){
+           System.out.println("The two LandTracts are equal.");
+        }
+        else{
+            System.out.println("The two LandTracts are not equal.");
+        }
         
     }
 }
--- LAB6/src/main/java/Month.java
+++ LAB6/src/main/java/Month.java
@@ -9,5 +9,120 @@
  * @author tpbig
  */
 public class Month {
-
+    private int MonthNumber;
+    
+    public Month(){
+        MonthNumber = 1;
+    }
+    public Month(int number){
+        if(number < 1 || number > 12){
+            MonthNumber = 1;
+        }else{
+            MonthNumber = number;
+        }
+    }
+    public Month(String name){
+        switch(name.toUpperCase()){
+            case "JANUARY":
+                MonthNumber = 1;
+                break;
+            case "FEBUARY":
+                MonthNumber = 2;
+                break;
+            case "MARCH":
+                MonthNumber = 3;
+                break;
+            case "APRIL":
+                MonthNumber = 4;
+                break;
+            case "MAY":
+                MonthNumber = 5;
+                break;
+            case "JUNE":
+                MonthNumber = 6;
+                break;
+            case "JULY":
+                MonthNumber = 7;
+                break;
+            case "AUGUST":
+                MonthNumber = 8;
+                break;
+            case "SEPTEMBER":
+                MonthNumber = 9;
+                break;
+            case "OCTOBER":
+                MonthNumber = 10;
+                break;
+            case "NOVEMBER":
+                MonthNumber = 11;
+                break;
+            case "DECEMBER":
+                MonthNumber = 12;
+                break;
+            default: MonthNumber = 1;
+                 break;
+        }
+    }
+    
+    public void setMonthNumber(int newNum){
+        if(newNum < 1 || newNum > 12){
+            MonthNumber = 1;
+        }else{
+            MonthNumber = newNum;
+        }
+    }
+    
+    public int getMonthNumber(){
+        return MonthNumber;
+    }
+    public String getMonthName(){
+        switch (MonthNumber) {
+            case 1:  return "January";
+            case 2:  return "February";
+            case 3:  return "March";
+            case 4:  return "April";
+            case 5:  return "May";
+            case 6:  return "June";
+            case 7:  return "July";
+            case 8:  return "August";
+            case 9:  return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+    
+        }
+        return "Void";
+    }
+    
+    @Override
+    public String toString(){
+        switch (MonthNumber) {
+            case 1:  return "January";
+            case 2:  return "February";
+            case 3:  return "March";
+            case 4:  return "April";
+            case 5:  return "May";
+            case 6:  return "June";
+            case 7:  return "July";
+            case 8:  return "August";
+            case 9:  return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+    
+        }
+        return "Void";
+    }
+    
+    public boolean equals(Month otherMonth){
+        return MonthNumber == otherMonth.getMonthNumber();
+    }
+    
+    public boolean greaterThan(Month otherMonth){
+        return MonthNumber > otherMonth.getMonthNumber();
+    }
+    public boolean lessThan(Month otherMonth){
+        return MonthNumber < otherMonth.getMonthNumber();
+    }
+    
 }
